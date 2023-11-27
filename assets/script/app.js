f96c25bff8952a558537d0728a31a2e9f7b53b1c
Dale Ruppert <daleruppert@shaw.ca>
2023-11-27 09:20:50 -0600

    as far as I can go

--- assets/script/app.js
+++ assets/script/app.js
@@ -87,11 +87,3 @@ app.use('/assets', express.static('assets', {
         }
     },
 }));
-
-// Other server configurations or routes can be added here
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
